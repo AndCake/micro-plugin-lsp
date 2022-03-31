d990d4679a73d9b1a28d82a7a54f0a5aea607d52
AndCake <roq@quixotic-worx.de>
2022-03-31 10:36:35 +0200

    :bug: fixes another small issue with text sometimes being torn apart
    - :pencil2: adds some more documentation

--- main.lua
+++ main.lua
@@ -388,27 +388,41 @@ function formatActionResponse(callback)
 	return function (bp, data)
 		if data.result == nil then return; end
 		local edits = data.result
+		-- make sure we apply the changes from back to front
+		-- this allows for changes to not need position updates
 		table.sort(edits, function (left, right)
+			-- go by lines first
 			return left.range['end'].line > right.range['end'].line or 
-				left.range['end'].line == right.range['end'].line and left.range['end'].character > right.range['end'].character
+				-- if lines match, go by end character
+				left.range['end'].line == right.range['end'].line and left.range['end'].character > right.range['end'].character or
+				-- if they match too, go by start character
+				left.range['end'].line == right.range['end'].line and left.range['end'].character == right.range['end'].character and left.range.start.line == left.range['end'].line and left.range.start.character > right.range.start.character
 		end)
 
+		-- save original cursor position
 		local xy = buffer.Loc(bp.Cursor.X, bp.Cursor.Y)
 		for _idx, edit in ipairs(edits) do
 			rangeStart = buffer.Loc(edit.range.start.character, edit.range.start.line)
 			rangeEnd = buffer.Loc(edit.range['end'].character, edit.range['end'].line)
+			-- apply each change
 			bp.Cursor:GotoLoc(rangeStart)
 			bp.Cursor:SetSelectionStart(rangeStart)
 			bp.Cursor:SetSelectionEnd(rangeEnd)
 			bp.Cursor:DeleteSelection()
 			bp.Cursor:ResetSelection()
-			bp.Buf:insert(rangeStart, edit.newText)
+			
+			if edit.newText ~= "" then
+				bp.Buf:insert(rangeStart, edit.newText)
+			end
 		end
+		-- put the cursor back where it was
 		bp.Cursor:GotoLoc(xy)
+		-- if any changes were applied
 		if #edits > 0 then
+			-- tell the server about the changed document
 			onRune(bp)
 		end
-		
+
 		if callback ~= nil then
 			callback(bp)
 		end
